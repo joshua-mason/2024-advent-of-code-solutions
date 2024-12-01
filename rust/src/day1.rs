2785e8096219b79302a78e3b0996873c028ea376
Joshua Mason <joshua.mark.mason95@gmail.com>
2024-12-01 17:49:43 +0000

    Improvements on first attempt (more secure handling for bad data)

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -14,20 +14,23 @@ fn read_input_file() -> String {
 }
 
 fn read_lines_to_integer_lists(data: String) -> (Vec<i32>, Vec<i32>) {
-    let lines = data.split("\n");
+    let lines = data.lines();
     let mut first_list: Vec<i32> = vec![];
     let mut second_list: Vec<i32> = vec![];
 
     for line in lines {
-        let mut split_line = line.split_whitespace();
-        let number_one_str = split_line.next();
-        let number_two_str = split_line.next();
+        let mut split_line = line.trim().split_whitespace();
 
-        let number_one = number_one_str.unwrap().parse::<i32>().unwrap();
-        let number_two = number_two_str.unwrap().parse::<i32>().unwrap();
-
-        first_list.push(number_one);
-        second_list.push(number_two);
+        if let (Some(n1), Some(n2)) = (split_line.next(), split_line.next()) {
+            if let (Ok(num1), Ok(num2)) = (n1.parse::<i32>(), n2.parse::<i32>()) {
+                first_list.push(num1);
+                second_list.push(num2);
+            } else {
+                println!("Failed to parse numbers on line: {}", line)
+            }
+        } else {
+            println!("Malformed line: {}", line)
+        }
     }
     (first_list, second_list)
 }
