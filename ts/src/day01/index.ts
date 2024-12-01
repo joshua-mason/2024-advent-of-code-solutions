350984ee1a8d13e700d881814b6ab2bdf1c959d2
Joshua Mason <joshua.mark.mason95@gmail.com>
2024-12-01 17:12:44 +0000

    Default styling changes

--- ts/src/day01/index.ts
+++ ts/src/day01/index.ts
@@ -2,86 +2,87 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 async function loadData() {
-    const file = fs.readFileSync(path.join(__dirname + "/data.txt"), undefined);
-    return file.toString()
+  const file = fs.readFileSync(path.join(__dirname + '/data.txt'), undefined);
+  return file.toString();
 }
 
 interface LocationIDs {
-    firstList: number[];
-    secondList: number[];
+  firstList: number[];
+  secondList: number[];
 }
 
 interface RepeatFrequency {
-    value: number;
-    occurrences: number;
+  value: number;
+  occurrences: number;
 }
 
 async function parseData(rawData: string): Promise<LocationIDs> {
-    const lines = rawData.split("\n");
-    const firstList: number[] = [];
-    const secondList: number[] = [];
+  const lines = rawData.split('\n');
+  const firstList: number[] = [];
+  const secondList: number[] = [];
 
-    lines.map(line => {
-        const [firstNumber, secondNumber] = line.split(/\s+/)
-        firstList.push(parseInt(firstNumber));
-        secondList.push(parseInt(secondNumber));
-    })
+  lines.map((line) => {
+    const [firstNumber, secondNumber] = line.split(/\s+/);
+    firstList.push(parseInt(firstNumber));
+    secondList.push(parseInt(secondNumber));
+  });
 
-    return {
-        firstList,
-        secondList
-    }
+  return {
+    firstList,
+    secondList
+  };
 }
 
 function sortData(data: LocationIDs): LocationIDs {
-    return {
-        firstList: [...data.firstList].sort(),
-        secondList: [...data.secondList].sort(),
-    }
+  return {
+    firstList: [...data.firstList].sort(),
+    secondList: [...data.secondList].sort()
+  };
 }
 
 function sumOfDiffs(data: LocationIDs): number {
-    let sum = 0;
-    for (let index = 0; index < data.firstList.length; index++) {
-        const n1 = data.firstList[index];
-        const n2 = data.secondList[index];
-        const diff = Math.abs(n1 - n2)
-        sum += diff;
-    }
-    return sum
+  let sum = 0;
+  for (let index = 0; index < data.firstList.length; index++) {
+    const n1 = data.firstList[index];
+    const n2 = data.secondList[index];
+    const diff = Math.abs(n1 - n2);
+    sum += diff;
+  }
+  return sum;
 }
 
-
 function getRepeatFrequencies(data: LocationIDs): RepeatFrequency[] {
-    const repeatedValues: RepeatFrequency[] = []
-    for (const firstListValue of data.firstList) {
-        const occurrences = data.secondList.filter(secondListValue => secondListValue === firstListValue).length
-        repeatedValues.push({
-            value: firstListValue,
-            occurrences: occurrences
-        })
-    }
-    return repeatedValues
+  const repeatedValues: RepeatFrequency[] = [];
+  for (const firstListValue of data.firstList) {
+    const occurrences = data.secondList.filter(
+      (secondListValue) => secondListValue === firstListValue
+    ).length;
+    repeatedValues.push({
+      value: firstListValue,
+      occurrences: occurrences
+    });
+  }
+  return repeatedValues;
 }
 
 function calculateSimilarityScore(repeatFrequencies: RepeatFrequency[]) {
-    let score = 0;
-    for (const frequency of repeatFrequencies) {
-        score += frequency.occurrences * frequency.value;
-    }
-    return score
+  let score = 0;
+  for (const frequency of repeatFrequencies) {
+    score += frequency.occurrences * frequency.value;
+  }
+  return score;
 }
 
 async function main() {
-    const rawData = await loadData();
-    const data = await parseData(rawData);
-    const sortedData = sortData(data)
-    const totalDistance = sumOfDiffs(sortedData)
-    console.log("Sum of sorted differences:", totalDistance)
+  const rawData = await loadData();
+  const data = await parseData(rawData);
+  const sortedData = sortData(data);
+  const totalDistance = sumOfDiffs(sortedData);
+  console.log('Sum of sorted differences:', totalDistance);
 
-    const repeatFrequencies = getRepeatFrequencies(data);
-    const similarityScore = calculateSimilarityScore(repeatFrequencies);
-    console.log("Similarity score: ", similarityScore);
+  const repeatFrequencies = getRepeatFrequencies(data);
+  const similarityScore = calculateSimilarityScore(repeatFrequencies);
+  console.log('Similarity score: ', similarityScore);
 }
 
-main()
+main();
