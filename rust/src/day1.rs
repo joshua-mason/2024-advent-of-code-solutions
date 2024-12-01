c4d1a8605ced4327d43a5506e370d6e4a42eacd8
Joshua Mason <joshua.mark.mason95@gmail.com>
2024-12-01 17:42:47 +0000

    Initial implementation parsing integer lists

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -1,3 +1,28 @@
 pub fn run() {
+    let data = std::fs::read_to_string("./inputs/day1.txt").unwrap();
+
     println!("Day 1 solution not implemented yet!");
+
+    let lines = data.split("\n");
+    let (first_list, second_list) = read_lines_to_integer_lists(lines);
+    println!("{:?}", first_list);
+    println!("{:?}", second_list);
+}
+
+fn read_lines_to_integer_lists(lines: std::str::Split<'_, &str>) -> (Vec<i32>, Vec<i32>) {
+    let mut first_list: Vec<i32> = vec![];
+    let mut second_list: Vec<i32> = vec![];
+
+    for line in lines {
+        let mut split_line = line.split_whitespace();
+        let number_one_str = split_line.next();
+        let number_two_str = split_line.next();
+
+        let number_one = number_one_str.unwrap().parse::<i32>().unwrap();
+        let number_two = number_two_str.unwrap().parse::<i32>().unwrap();
+
+        first_list.push(number_one);
+        second_list.push(number_two);
+    }
+    (first_list, second_list)
 }
