1c5f67925697ea3e9eb77fec3434afade4afeda9
Joshua Mason <joshua.mark.mason95@gmail.com>
2024-12-03 08:24:50 +0000

    Day 3: part 2

--- ts/src/day03/index.ts
+++ ts/src/day03/index.ts
@@ -5,25 +5,52 @@ const dataFilePath = __dirname + '/data.txt';
 
 async function day3() {
   const rawData = await loadData(dataFilePath);
-  const mulPairs = matchAllMuls(rawData);
-  const total = aggregateMulPairs(mulPairs);
-  console.log('Total:', total);
+  const mulPairsNoInstructions = matchAllMultiplications(rawData, false);
+  const totalNoInstructions = aggregateMulPairs(mulPairsNoInstructions);
+  console.log('Total without instructions:', totalNoInstructions);
+  const mulPairsInstructions = matchAllMultiplications(rawData, true);
+  const totalInstructions = aggregateMulPairs(mulPairsInstructions);
+  console.log('Total with instructions:', totalInstructions);
 }
 
 day3();
 
 type MulPairs = [number, number][];
 
-function matchAllMuls(data: string) {
-  const regex = /mul\(([0-9][0-9]?[0-9]?),([0-9][0-9]?[0-9]?)\)+/g;
+function matchAllMultiplications(
+  data: string,
+  followInstructions: boolean = false,
+) {
+  const regex = createInstructionRegex(followInstructions);
+
   const mulPairs: MulPairs = [];
   let match;
+  let aggregating = true;
   while ((match = regex.exec(data))) {
-    const [_, n1Str, n2Str] = match;
-    mulPairs.push([parseInt(n1Str), parseInt(n2Str)]);
+    const [matchedString, n1Str, n2Str] = match;
+    if (matchedString === 'do()') {
+      aggregating = true;
+      continue;
+    }
+    if (matchedString === "don't()") {
+      aggregating = false;
+      continue;
+    }
+    if (aggregating) mulPairs.push([parseInt(n1Str), parseInt(n2Str)]);
   }
 
   return mulPairs;
+}
+
+function createInstructionRegex(followInstructions: boolean) {
+  const mulFunctionPattern = /mul\(([0-9][0-9]?[0-9]?),([0-9][0-9]?[0-9]?)\)/
+    .source;
+  const instructionFunctionPattern =
+    /|/.source + /do\(\)/.source + /|/.source + /don\'t\(\)/.source;
+  const regex = followInstructions
+    ? new RegExp(mulFunctionPattern + instructionFunctionPattern, 'g')
+    : new RegExp(mulFunctionPattern, 'g');
+  return regex;
 }
 
 function aggregateMulPairs(mulPairs: MulPairs) {
